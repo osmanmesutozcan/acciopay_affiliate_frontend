e2f80847ba7890f32c05cd4c92598a5e990ce3a2
Alina Zhusupbekova <zhusupbekovalina@gmail.com>
2022-06-29 10:07:24 +0600

    sentry auth error fix

--- next.config.js
+++ next.config.js
@@ -38,7 +38,7 @@ const sentryWebpackPluginOptions = {
   // recommended:
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
-
+  dryRun: process.env.VERCEL_ENV !== "production" || process.env.VERCEL_ENV !== "development",
   silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
